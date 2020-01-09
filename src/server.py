4d95b1ea6d989e29a48feb7c0137d658808f7a88
Joshua Korver <Joshua.Korver@ampf.com>
2020-01-09 08:06:54 -0600

    can now login a Client with a udp connection.

--- src/server.py
+++ src/server.py
@@ -12,54 +12,52 @@ PROTOCOL_VERSION = '1'
 
 class Client:
   
-  def __init__(self, connection, address):
+  def __init__(self, address):
     self.input_messages = []
     self.output_messages = []
     self.disconnect = False
     self.player = None
-    self.connection = connection
     self.address = address
     self.last_input = time()
-    self.messages = {
-      SERVER_COMMANDS.login: Client.do_login,
-      SERVER_COMMANDS.logout: Client.do_logout
-    }
   
   def read_input(self):
+    print('boo')
     # read all bytes into an input_buffer
-    input_data = self.connection.recv(2048)
-    if len(input_data) > 1024:
-      print('Client sending too much data. Closing client:', self.address)
-      self.disconnect = True
-      input_data = b''
+    #input_data = self.connection.recv(2048)
+    #if len(input_data) > 1024:
+      #print('Client sending too much data. Closing client:', self.address)
+      #self.disconnect = True
+      #input_data = b''
     # parse input_buffer into messages
-    while len(input_data):
-      self.last_input = time()
-      message = Message()
-      input_data = message.parse(input_data)
-      self.input_messages.append(message)
+    #while len(input_data):
+      #self.last_input = time()
+      #message = Message()
+      #input_data = message.parse(input_data)
+      #self.input_messages.append(message)
   
   def send_output(self):
+    self.output_messages.clear()
     # encode output_messages into an output_buffer
-    output_buffer = ''
-    while len(self.output_messages):
-      message = self.output_messages.pop(0)
-      output_buffer += message.encode()
+    #output_buffer = ''
+    #while len(self.output_messages):
+      #message = self.output_messages.pop(0)
+      #output_buffer += message.encode()
     # send output_buffer to client as bytes
-    if output_buffer:
-      encoded_output = output_buffer.encode()
-      print('Output from:', self.address, 'data:', encoded_output)
-      self.connection.sendall(encoded_output)
+    #if output_buffer:
+      #encoded_output = output_buffer.encode()
+      #print('Output from:', self.address, 'data:', encoded_output)
+      #self.connection.sendall(encoded_output)
 
   def step(self):
     if len(self.input_messages):
       message = self.input_messages.pop(0)
-      if message.action in self.messages:
-        (self.messages[message.action])(self, message.args)
-      elif self.player:
-        self.player.do_command(message)
-        self.output_messages.extend(self.player.output_queue)
-        self.player.output_queue.clear()
+      self.output_messages.append(message)
+      #if message.action in self.messages:
+      #  (self.messages[message.action])(self, message.args)
+      #elif self.player:
+      #  self.player.do_command(message)
+      #  self.output_messages.extend(self.player.output_queue)
+      #  self.player.output_queue.clear()
   
   def do_login(self, args):
     if not len(args) == 3:
@@ -94,7 +92,7 @@ class Client:
     print('saving player and closing: {}'.format(self.address))
     with shelve.open('data/users') as users:
       users[self.player.username] = self.player
-    self.connection.close()
+    #self.connection.close()
 
 class Server:
   def __init__(self, address, timeout=3):
@@ -104,17 +102,36 @@ class Server:
     self.socket = None
     self.clients = {}
   
-  def __process_connect(self, socket):
-    print('socket:', socket)
-    address = socket.laddr
-    print('address:', address)
-    client = Client(address)
-    self.clients[address] = client
-    #self.selector.register(connection, selectors.EVENT_READ, data=client)
-    print('Initial connection from', address)
-    print('Total Clients:', len(self.clients))
+  def __login_client(self, address, args):
+    if not len(args) == 3:
+      print('Malformed login request from', address)
+      # TODO reply with error
+      return
+    username = args[0]
+    password = args[1]
+    version = args[2]
+    if not version == PROTOCOL_VERSION:
+      print('login request with bad version from', address)
+      # TODO reply with error
+      return
+    with shelve.open('data/users') as users:
+      if not username in users:
+        print('User not found:', username)
+        # TODO reply with error
+      else:
+        player = users[username]
+        if not player.check_password(password):
+          print('Bad credentials for "', username, '"')
+          # TODO reply with error
+        else:
+          print('Logging in "', username, '" from', address)
+          client = Client(address)
+          client.player = player
+          self.clients.update({address: client})
+          print('active connections: ', self.clients)
+          # TODO reply with success
   
-  def __process_disconnect(self, client):
+  def __disconnect_client(self, client):
     print('disconnecting:', client.address)
     self.selector.unregister(client.connection)
     client.close()
@@ -141,17 +158,21 @@ class Server:
       print('key:', key)
       print('mask:', mask)
       socket = key.fileobj
-      data, address = socket.recvfrom('2048')
-      if self.clients[address] is None:
+      data, address = socket.recvfrom(2048)
+      client = self.clients.get(address)
+      if client is None:
         print('need to login the new address')
         # todo attempt login
-        #self.__process_connect(socket)
+        command = Message()
+        data = command.parse(data)
+        if command.action == SERVER_COMMANDS.login:
+          self.__login_client(address, command.args)
       else:
         try:
-          self.clients[address].read_input()
+          client.read_input()
         except OSError:
           print('error reading from', address)
-          self.clients.remove(address)
+          self.clients.pop(address)
   
   def send(self):
     for client in self.clients.values():
